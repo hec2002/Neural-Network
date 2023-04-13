c115e271d7f047009885e10591ade83c48b6756b
Hector Hurtado <hlh94@cornell.edu>
2023-04-13 18:28:25 -0400

    Cleaned up code by creating module.py

--- model.py
+++ model.py
@@ -1,35 +1,22 @@
-# modular implementation design allows for ease in model creation
-from cmath import tanh
-from re import X
-from dense import Dense
-from tanh import Tanh
-from losses import mse, mse_prime
-import numpy as np
-X = np.reshape([[0, 0], [0, 1], [1, 0], [1, 1]], (4, 2, 1))
-Y = np.reshape([[0], [1], [1], [0]], (4, 1, 1))
-network = [
-    Dense(2, 3),
-    Tanh(),
-    Dense(3, 1),
-    Tanh()
-]
-# hyperparameters
-epochs = 1000
-learning_rate = 0.1
+def prediction(network, output):
+    for layer in network:
+        output = layer.forward(output)
+    return output
+def train(network, loss, loss_prime, x_train, y_train, epochs = 1000, learning_rate = 0.01, print_error = True):
+    for e in range(epochs):
+        error = 0
+        for x, y in zip(x_train, y_train):
+            # forward
+            output = prediction(network, x)
 
-# train
-for e in range(epochs):
-    error = 0
-    # forward propagation
-    for x, y in zip(X, Y):
-        output = x
-        for layer in network:
-            output = layer.forward(output)
-        # error
-        error += mse(y, output)
-        # backpropagation
-        gradient = mse_prime(y, output)
-        for layer in reversed(network):
-            gradient = layer.backward(gradient, learning_rate)
-    error /= len(X)
-    print('%d/%d, error=%f' % (e + 1, epochs, error))
+            # error
+            error += loss(y, output)
+
+            # backward
+            gradient = loss_prime(y, output)
+            for layer in reversed(network):
+                gradient = layer.backward(gradient, learning_rate)
+
+        error /= len(x_train)
+        if print_error:
+            print(f"{e + 1}/{epochs}, error={error}")
